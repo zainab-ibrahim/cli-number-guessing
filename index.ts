b7b92117e129dcb91fdb834870f821ec093ddb10
zainabibrahim <zainabjadgal166@gmail.com>
2024-04-22 21:25:12 +0500

    added a console

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random()  *10 + 1);
 
+console.log("Welcome to number Guessing Game");
 
 const answers = await inquirer.prompt([
     {
